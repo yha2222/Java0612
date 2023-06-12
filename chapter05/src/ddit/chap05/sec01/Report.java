ec38d5e32d246b9af8b6d79448553a3189958c02
PC-22 <PC-22@DESKTOP-VKHVS2N>
2023-06-12 19:31:17 +0900

    test UTF-8

---
+++ chapter05/src/ddit/chap05/sec01/Report.java
@@ -0,0 +1,12 @@
+package ddit.chap05.sec01;
+
+public class Report {
+
+	public static void main(String[] args) {
+		//문제 1] 정수 5개를 저장하고 역순으로 인쇄    
+		//문제 2] 저장된 10개의 점수 중 최대값과 최소값을 구하시오
+	    //문제 3] 5명의 이름을 배열에 저장한 후 첫 번째 이름과 마지막 이름을 서로 바꾸시오
+
+	}
+
+}
