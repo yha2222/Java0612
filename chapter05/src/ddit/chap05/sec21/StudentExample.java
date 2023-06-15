1e6dfd81e037b8d21d5514100df69dabc29f8af9
PC-22 <PC-22@DESKTOP-VKHVS2N>
2023-06-15 15:12:33 +0900

    0615

---
+++ chapter05/src/ddit/chap05/sec21/StudentExample.java
@@ -0,0 +1,19 @@
+package ddit.chap05.sec21;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+		//Student st = new Student("이성계", 24, false); 
+		//클래스 객체 주소 ->st => this!
+		//System.out.println(st); => ddit.chap05.sec21.Student@15db9742
+		
+//		Student st[] = new Student[28];
+//		st[0] = new Student("심우식", 30, false);
+		Student[] st = {new Student("심우식", 30, false),
+						new Student("이상민", 27, false),
+						new Student("전유빈", 25, false),
+						new Student("지윤서", 26, false),
+				};
+	}
+
+}
